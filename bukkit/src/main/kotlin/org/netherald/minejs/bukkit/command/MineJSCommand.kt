29472d725d016e4993cfe3ed24ec123c13a7128d
myoun <aiden080605@gmail.com>
2021-08-12 14:32:42 +0900

    command error fix

--- bukkit/src/main/kotlin/org/netherald/minejs/bukkit/command/MineJSCommand.kt
+++ bukkit/src/main/kotlin/org/netherald/minejs/bukkit/command/MineJSCommand.kt
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.netherald.minejs.bukkit.MineJsBukkit
 import org.netherald.minejs.bukkit.impl.ConsoleImpl
+import org.netherald.minejs.bukkit.impl.ItemManagerImpl
 import org.netherald.minejs.bukkit.impl.PlayerManagerImpl
 import org.netherald.minejs.common.Platform
 import org.netherald.minejs.common.ScriptLoader
@@ -20,7 +21,7 @@ class MineJSCommand(private val plugin: MineJsBukkit) : CommandExecutor {
         if (args[0] == "reload") {
             sender.sendMessage(Component.text("${ChatColor.RED}Loading scripts..."))
             ScriptLoader.unload()
-            ScriptLoader.load(plugin.scriptsDir, Platform.BUKKIT, PlayerManagerImpl(), ConsoleImpl(plugin))
+            ScriptLoader.load(plugin.scriptsDir, Platform.BUKKIT, PlayerManagerImpl(),ItemManagerImpl() ,ConsoleImpl(plugin))
             sender.sendMessage(Component.text("${ChatColor.GREEN}Complete loading scripts"))
 
             return true
