7a9ec4cc464ffaa2ec5ff04fbd6a19211b768c9a
myoun <aiden080605@gmail.com>
2021-08-12 11:31:43 +0900

    cleaning build.gradle.kts removed some dirty repositories and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,74 +1,17 @@
 plugins {
-    kotlin("jvm") version "1.5.20"
+    kotlin("jvm") version "1.5.21"
 }
 
-group = "org.netherald"
-version = "1.0.0"
-
-repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
-    maven("https://jitpack.io")
-    //maven("https://repo.dmulloy2.net/repository/public/")
-}
-
-dependencies {
-    implementation(kotlin("stdlib"))
-    compileOnly("io.papermc.paper:paper-api:1.17-R0.1-SNAPSHOT")
-    //compileOnly("com.comphenix.protocol:ProtocolLib:4.6.0")
-}
-
-subprojects {
-    apply {
-        plugin("org.jetbrains.kotlin.jvm")
-    }
-
+allprojects {
     repositories {
         mavenCentral()
     }
-
-    dependencies {
-        implementation(kotlin("stdlib-jdk8"))
-    }
 }
 
-val shade = configurations.create("shade")
-shade.extendsFrom(configurations.implementation.get())
+subprojects {
+    apply(plugin="org.jetbrains.kotlin.jvm")
 
-tasks {
-
-    javadoc {
-        options.encoding = "UTF-8"
-    }
-
-    compileJava {
-        options.encoding = "UTF-8"
-    }
-
-    compileKotlin {
-        kotlinOptions.jvmTarget = "16"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "16"
-    }
-
-    create<Jar>("sourceJar") {
-        archiveClassifier.set("source")
-        from(sourceSets["main"].allSource)
-    }
-
-    jar {
-        from (shade.map { if (it.isDirectory) it else zipTree(it) })
-    }
-
-    // From monun/tap-sample-plugin
-    create<Copy>("copyToServer") {
-        from(jar)
-        val plugins = File(rootDir, ".server/plugins")
-        if (File(shade.artifacts.files.asPath).exists()) {
-            into(File(plugins, "update"))
-        } else {
-            into(plugins)
-        }
+    dependencies {
+        implementation(kotlin("stdlib"))
     }
 }
--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,18 +1,37 @@
-plugins {
-}
-
-group = "org.example"
-version = "1.0.0"
-
-repositories {
-    mavenCentral()
-    mavenLocal()
-}
-
 dependencies {
     implementation(project(":common"))
     implementation("com.eclipsesource.j2v8:j2v8:6.2.0")
     implementation("com.eclipsesource.j2v8:j2v8_win32_x86_64:4.6.0")
     implementation("com.eclipsesource.j2v8:j2v8_linux_x86_64:4.6.0")
     implementation("com.eclipsesource.j2v8:j2v8_macosx_x86_64:4.6.0")
+}
+
+val shade = configurations.create("shade")
+shade.extendsFrom(configurations.implementation.get())
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "16"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "16"
+    }
+    jar {
+        from(shade.map {if (it.isDirectory) it else zipTree(it)})
+    }
+
+    create<Jar>("sourceJar") {
+        archiveClassifier.set("source")
+        from(sourceSets["main"].allSource)
+    }
+
+    create<Copy>("copyToServer") {
+        from(jar)
+        val plugins = File(rootDir, ".server/plugins")
+        if (File(shade.artifacts.files.asPath).exists()) {
+            into(File(plugins, "update"))
+        } else {
+            into(plugins)
+        }
+    }
 }
